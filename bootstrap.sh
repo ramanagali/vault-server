75f559d2e129f158b8b74af7eb9f0062b924674f
ramanagali <ramana.gali@gmail.com>
2022-04-26 14:44:29 +0800

    updated vault scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,5 @@ vagrant up
 
 export VAULT_ADDR='http://127.0.0.1:8200'
 
-
-#update the vagrant cluster
-#vagrant ssh vault-node -- -t 'sudo swapoff -a && sudo systemctl restart kubelet'
-#------------------------------------------------------------------------------------
 echo "*** vault-node is ready to use ****"
-
+#------------------------------------------------------------------------------------
